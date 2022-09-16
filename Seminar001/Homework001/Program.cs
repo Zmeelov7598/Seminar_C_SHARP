8497a6b7a5e76b905d5334b082734a26af1ecadc
SERGEY <Zmeelov7598@yandex.ru>
2022-09-16 13:17:51 +0300

    Сделано Первое домашнее задание

--- Seminar001/Homework001/Program.cs
+++ Seminar001/Homework001/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число!");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число!");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"MIN={b}");
+    Console.WriteLine($"MAX={a}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"MIN={a}");
+    Console.WriteLine($"MAX={b}");
+}
+else if (a == b)
+{
+    Console.WriteLine("Вы ввели два одинаковых числа! MIN=MAX");
+}
