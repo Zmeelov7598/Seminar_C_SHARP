84b08bf7f6a7456fc3e4aa5bea564f34855e6e42
SERGEY <Zmeelov7598@yandex.ru>
2022-09-16 13:44:31 +0300

    Сдклана вторая задача

--- Seminar001/Homework002/Program.cs
+++ Seminar001/Homework002/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число!");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число!");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число!");
+int c = Convert.ToInt32(Console.ReadLine());
+int Max = a;
+if (b > Max)
+{
+    Max = b;
+}
+else if (c > Max)
+{
+    Max = c;
+}
+Console.WriteLine($"Max={Max}");
