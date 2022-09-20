cd4c27f4ab9fb6d938e6aa45bbdcaff3364e707b
SERGEY <Zmeelov7598@yandex.ru>
2022-09-20 23:22:32 +0300

    Ferst Decision

--- Seminar002/HomeWork001/Program.cs
+++ Seminar002/HomeWork001/Program.cs
@@ -1,5 +1,15 @@
-﻿Console.Write("Введите трехзначное число: ");
-int ThreeDigitNumber = int.Parse(Console.ReadLine());
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Write("Введите трехзначное число: ");
+int ThreeDigitNumber = int.Parse(Console.ReadLine() ?? "0");
+int[] arrayTDN = new int[3];
 
-Console.WriteLine(ThreeDigitNumber);
+for (int i = 2; i >= 0; i--)
+{
+    arrayTDN[i] = ThreeDigitNumber % 10;
+    ThreeDigitNumber /= 10;
+}
 
+Console.WriteLine(arrayTDN[1]);
