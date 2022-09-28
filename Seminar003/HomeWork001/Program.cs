a5d2f361d70623d03ea40998895d5a220fdd8ce6
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-09-28 09:00:42 +0300

    Finish First HomeWork

---
+++ Seminar003/HomeWork001/Program.cs
@@ -0,0 +1,29 @@
+﻿int Palindrom(int number)
+{
+    int n = number;
+    int rev = 0;
+    while (rev <= 9999)
+    {
+        int ost = n % 10;
+        rev = rev * 10 + ost;
+        n = n / 10;
+    }
+    return (rev);
+}
+
+void CheckPalindrom(int i, int j)
+{
+    if (i == j)
+    {
+        Console.WriteLine("Да это число палиндром!");
+    }
+    else 
+    {
+        Console.WriteLine("НЕТ это число не палиндром!");
+    }
+}
+
+Console.Write("Введите пятизначное число : ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int revers = Palindrom(number);
+CheckPalindrom(number, revers);
