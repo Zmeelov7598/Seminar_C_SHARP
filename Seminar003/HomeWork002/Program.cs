7ec162726fc3276825b72056d982b25d93be7f95
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-09-28 09:55:37 +0300

    Finish HomeWork002

--- Seminar003/HomeWork002/Program.cs
+++ Seminar003/HomeWork002/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿double GetDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    return distance;
+}
+double GetNumber(string name)
+{
+    Console.WriteLine("Введите координату " + name);
+    return Convert.ToDouble(Console.ReadLine());
+}
+double x1 = GetNumber("x1");
+double y1 = GetNumber("y1");
+double z1 = GetNumber("z1");
+double x2 = GetNumber("x2");
+double y2 = GetNumber("y2");
+double z2 = GetNumber("z2");
+Console.WriteLine("Растояние между точками = " + GetDistance(x1, y1, z1, x2, y2, z2));
