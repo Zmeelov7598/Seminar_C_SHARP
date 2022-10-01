4e0d27d576f56d36f290bb3c175cfde5c81210d9
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-10-01 17:32:19 +0300

    Finish HomeWork001

---
+++ Seminar004/HomeWork001/Program.cs
@@ -0,0 +1,23 @@
+﻿int GetNumberA()
+{
+    Console.WriteLine("Введите число А!");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int GetNumberB()
+{
+    Console.WriteLine("Введите число В!");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int DegreeCalc(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+int A = GetNumberA();
+int B = GetNumberB();
+
+Console.WriteLine("Число " + A + " в степени "  + B +  " равно " + DegreeCalc(A,B));
