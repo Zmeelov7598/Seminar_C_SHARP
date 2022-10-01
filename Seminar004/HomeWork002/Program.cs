9cbc59d84e120edac2832e7326866ac2a3536c0e
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-10-01 18:06:55 +0300

    Finish HomeWork002

---
+++ Seminar004/HomeWork002/Program.cs
@@ -0,0 +1,28 @@
+﻿int GetNumber()
+{
+    Console.WriteLine("Введите число!");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int Len(int a)
+{
+    int i = 0;
+    while (a != 0)
+    {
+        a = a / 10;
+        i++;
+    }
+    return i;
+}
+
+int Sum(int a, int len)
+{
+    int result = 0;
+    for (int i = 0; i <= len; i++)
+    {
+        result = result + (a % 10);
+        a = a / 10;
+    }
+    return result;
+}
+int N = GetNumber();
+Console.WriteLine(Sum(N, Len(N)));
