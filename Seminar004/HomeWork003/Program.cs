b2263b36d29ef0040b65414007e93dbf89ee0e0e
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-10-01 18:27:24 +0300

    Finish HomeWork003

---
+++ Seminar004/HomeWork003/Program.cs
@@ -0,0 +1,23 @@
+﻿int GetNumber()
+{
+    Console.WriteLine("Введите чсило обозначающее длинну массива!");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void CreatArray(int a)
+{
+    int[] array = new int[a];
+    for (int i = 0; i < a; i++)
+    {
+        if (i == a-1)
+        {
+          array[i] = new Random().Next(0,21);
+        Console.Write(array[i]); 
+        Console.WriteLine();
+        break;
+        }
+
+        array[i] = new Random().Next(0,21);
+        Console.Write(array[i] + ",");
+    }
+}
+CreatArray(GetNumber());
