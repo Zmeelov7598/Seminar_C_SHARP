40d95cacca22dc8cde79257c281c4b8778947d1b
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-10-09 19:00:36 +0600

    Finish HomeWork001

---
+++ Seminar007/HomeWork001/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двухмерный массив размером mxn
+// Заполненный случайными вещественными числами
+
+double[,] DemensionArray()
+{
+    Console.WriteLine("Введите количество строк массива :");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов массива : ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    double[,] array = new double[m, n];
+    return array;
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().NextDouble() * 10, 1);
+        }
+    }
+}
+
+double[,] massiv = DemensionArray();
+Console.WriteLine();
+PrintArray(massiv);
+FillArray(massiv);
+Console.WriteLine();
+PrintArray(massiv);
