4328a27e96df119badd3bb7bbcc85a36cccaabe6
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-10-14 20:53:01 +0600

    Finish HomeWork001

---
+++ Seminar009/HomeWork001/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значение N.Напишите программу, к
+// которая выведет все натуральные числа в промежутке от N до 1.
+// выполнить с помощью рекрсии
+
+void Sequence(double number)
+{
+    if (number > 1)
+    {
+        Console.Write($"{number},");
+        Sequence(number - 1);
+    }
+    else
+    {
+        Console.Write(number);
+        Console.WriteLine();
+    }
+        
+}
+
+double GetNumber()
+{
+    Console.WriteLine("Введите число N");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+Sequence(Math.Truncate(GetNumber()));
