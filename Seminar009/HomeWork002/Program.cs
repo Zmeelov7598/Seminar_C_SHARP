9fee6d38ea65da6d11e035aac22673f41ae936d5
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-10-14 21:41:58 +0600

    Finish HomeWork002

--- Seminar009/HomeWork002/Program.cs
+++ Seminar009/HomeWork002/Program.cs
@@ -1,2 +1,26 @@
 ﻿// Задайте значения M и N. Напишите программу,
 // которая найдет сумму натуральных элементов в промежутке от M до N
+
+double SumFor(double firstNumber, double lastNumber)
+{
+    double result = 0;
+    if (firstNumber < lastNumber)
+    {
+        result = firstNumber + SumFor(firstNumber + 1, lastNumber);
+    }
+    else if (firstNumber == lastNumber)
+    {
+         result = result + lastNumber;
+    }
+    return result;
+}
+
+double GetNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double m = Math.Ceiling(GetNumber("Введите число M : "));
+double n = Math.Truncate(GetNumber("Введите число N : "));
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} = {SumFor(m, n)}");
